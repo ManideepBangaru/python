70827cbbd373bcfeb6843c9a2acf8fa559ebe3ef
ManideepBangaru <bmd994@gmail.com>
2024-12-23 23:44:48 +0530

    add examples for unpacking arguments with lists, generators, and dictionaries

--- 06_Function_Argument_unpacking.py
+++ 06_Function_Argument_unpacking.py
@@ -8,3 +8,13 @@ list_vec = [1, 0, 1]
 
 print_vector(tuple_vec[0], tuple_vec[1], tuple_vec[2]) # not pythonic  # ugly
 print_vector(*tuple_vec)  # Pythonic  # better
+print_vector(*list_vec)  # Pythonic  # better
+
+# ---------------------------------------------------------------------------
+
+gen_expr = (x * x for x in range(3))
+print_vector(*gen_expr)  # Pythonic  # better
+
+dict_vec = {'y': 0, 'z': 1, 'x': 1}
+print_vector(**dict_vec)  # Pythonic  # better
+print_vector(*dict_vec)  # Pythonic  # better
