723c55566ca294813d9bd95915ddd3e6d236ab85
ManideepBangaru <bmd994@gmail.com>
2024-12-25 23:43:34 +0530

    refactor validation logic and introduce custom exception classes for name validation

--- 10_define_exception_classes.py
+++ 10_define_exception_classes.py
@@ -1,16 +1,36 @@
-# def validate(name):
-#     if len(name) < 10:
-#         raise ValueError("Name is too short")
+def validate(name):
+    if len(name) < 10:
+        raise ValueError("Name is too short")
 
 # validate("John")
 
 # --------------------------------------------------------
+# class NameTooShortError(ValueError):
+#     pass
 
-class NameTooShortError(ValueError):
-    pass
+# def validate(name):
+#     if len(name) < 10:
+#         raise NameTooShortError(name)
 
+# validate("Jane")
+
+# --------------------------------------------------------
 def validate(name):
     if len(name) < 10:
-        raise NameTooShortError(name)
+        raise ValueError("Name is too short")
+    if len(name) > 20:
+        raise NameTooLongError(name)
 
-validate("Jane")
+class BaseValidationError(ValueError):
+    pass
+class NameTooShortError(BaseValidationError):
+    pass
+class NameTooLongError(BaseValidationError):
+    pass
+class NameTooCuteError(BaseValidationError):
+    pass
+
+try:
+    validate("John wick")
+except BaseValidationError as err:
+    handle_validation_error(err)
