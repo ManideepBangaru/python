f79244a43c840f9674ad9cd7bc17ed48c1f7b8ce
ManideepBangaru <bmd994@gmail.com>
2024-12-30 22:18:59 +0530

    add example demonstrating subclassing and built-in methods of named tuples in Python

--- 14_named_tuples.py
+++ 14_named_tuples.py
@@ -14,3 +14,32 @@ my_car = Car("red", 3812.4) # Creating a new instance of the Car class
 print(my_car.color) # Accessing the fields using dot notation
 print(my_car.mileage) # Accessing the fields using dot notation
 print(my_car[0]) # Accessing the color field using index
+
+# Subclassing named tuples --------------------------------------------------
+Car = namedtuple('Car', 'color mileage')
+
+class MyCarWithMethods(Car):
+    def hexcolor(self):
+        if self.color == 'red':
+            return '#ff0000'
+        else:
+            return '#000000'
+
+c = MyCarWithMethods('red', 1234)
+print(c.hexcolor())
+
+Car = namedtuple('Car', 'color mileage')
+ElectricCar = namedtuple('ElectricCar', Car._fields + ('charge',))
+
+ev = ElectricCar('red', 1234, 45.0)
+print(ev.charge)
+
+#  Built-in helper methods --------------------------------------------------
+print(my_car._asdict())
+
+import json
+json_op = json.dumps(my_car._asdict())
+print(json_op)
+
+my_car._replace(color='blue')
+print(my_car)
